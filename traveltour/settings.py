6943d4a8a676ca94500f1de4a9a37b571557a415
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-08-31 22:20:53 +0530

    endcommit

--- traveltour/settings.py
+++ traveltour/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-%rbroi3dvpn*yospwa#hb9&*fggq7(+$)21r64*o^lnaiyzxs9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '0.0.0.0',
+    'heeling-minds.herokuapp.com', # your herokuapp url
+    '127.0.0.1']
 
 
 # Application definition
