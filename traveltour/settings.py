59e461f2a783599d1560688a3cc589a69ffe6281
triptiii3 <111265499+triptiii3@users.noreply.github.com>
2022-08-31 20:14:58 +0530

    add

--- traveltour/settings.py
+++ traveltour/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT=os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
